1d15eaa7ff3b84871fb00ca5f8ae6bf6244c09e6
Juraj Ondruska <juraj.ondr@gmail.com>
2022-04-22 15:48:21 +0200

    admin: add class MathHelper

---
+++ src/main/java/org/matol/MathHelper.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 Juraj Ondruska (juraj.ondr@gmail.com) to Present
+ * All rights reserved. Use is subject to license terms.
+ */
+
+package org.matol;
+
+/**
+ * @author Juraj Ondruska (juraj.ondr@gmail.com)
+ * @since 1.0.0
+ */
+public class MathHelper {
+
+    public Integer plus(Integer a, Integer b) {
+        return a + b;
+    }
+}
